3923cd2c634089acb5b0ba718e2dc17608a8833d
BoltzBit <mateus_desktop@outlook.com>
2024-08-12 22:00:23 -0300

    add: Enums for Budget and BudgetItem

---
+++ src/Budget.Core/Domain/Enuns/BudgetItemType.cs
@@ -0,0 +1,7 @@
+namespace Budget.Core.Domain.Enuns;
+
+public enum BudgetItemType
+{
+    Labor = 5,
+    Product = 10
+}
---
+++ src/Budget.Core/Domain/Enuns/BudgetType.cs
@@ -0,0 +1,8 @@
+namespace Budget.Core.Domain.Enuns;
+
+public enum BudgetType
+{
+    Labor = 5,
+    Material = 10,
+    Both = Labor + Material
+}
