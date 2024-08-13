5d5ee293b59d4b95976d0e252dff416e6f6de507
BoltzBit <mateus_desktop@outlook.com>
2024-08-12 23:56:37 -0300

    add: project context

---
+++ src/Budget.Infra/BudgetDbContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Budget.Infra;
+
+public class BudgetDbContext : DbContext
+{
+    public BudgetDbContext(DbContextOptions<BudgetDbContext> options)
+        :base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(BudgetDbContext).Assembly);
+    }
+
+    public override async Task<int> SaveChangesAsync(CancellationToken cancellationToken = new CancellationToken())
+    {
+        int result = await base.SaveChangesAsync(cancellationToken).ConfigureAwait(false);
+        return result;
+    }
+
+    public override int SaveChanges()
+    {
+        return SaveChangesAsync().GetAwaiter().GetResult();
+    }
+}
