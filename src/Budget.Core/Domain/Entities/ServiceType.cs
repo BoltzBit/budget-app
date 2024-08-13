2df4c67df7b700dfba6875593f23b58552f7d19d
BoltzBit <mateus_desktop@outlook.com>
2024-08-12 21:56:38 -0300

    add: Service Type entity

---
+++ src/Budget.Core/Domain/Entities/ServiceType.cs
@@ -0,0 +1,49 @@
+using Budget.Core.Domain.Entities;
+using Budget.Core.Domain.Utils;
+
+namespace Budget.Domain.Entities;
+
+public class ServiceType : BaseEntity
+{
+    public int ProviderId { get; private set; }
+    public string Name { get; private set; }
+    public string Description { get; private set; }
+
+    protected ServiceType(){}
+
+    public void Update(ServiceType serviceType)
+    {
+        if(serviceType.ProviderId != ProviderId)
+        {
+            ProviderId = serviceType.ProviderId;
+        }
+
+        if(serviceType.Name != Name)
+        {
+            Name = serviceType
+                .Name
+                .ValidateString(2, 100);
+        }
+
+        if(serviceType.Description != Description)
+        {
+            Description = serviceType
+                .Description
+                .ValidateString(2, 2000);
+        }
+    }
+
+    public ServiceType Create(
+        int providerId,
+        string name,
+        string description
+    )
+    {
+        return new ServiceType
+        {
+            ProviderId = providerId.ValidateInt(),
+            Name = name.ValidateString(2, 100),
+            Description = description.ValidateString(2, 2000)
+        };
+    }
+}
